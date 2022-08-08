f6234aa73148197d3ac027badf65dc3594ed6d1d
brayan.barrios <brayan.barrios@patagonian.it>
2022-08-08 09:39:26 -0500

    style: :art: update code structure

--- src/module/awsmessage/mensaje.module.ts
+++ src/module/awsmessage/mensaje.module.ts
@@ -1,29 +1,29 @@
 import { Module } from '@nestjs/common';
 import { SqsModule } from '@ssut/nestjs-sqs';
-import { MessageHandler } from './MessageHandler';
+import { MessageHandler } from './message.handler';
 import * as AWS from 'aws-sdk';
 import { config } from '../../config';
 import { RasterModule } from '../../raster/raster.module';
 
 AWS.config.update({
-    region: config.AWS_REGION,
-    accessKeyId: config.ACCESS_KEY_ID,
-    secretAccessKey: config.SECRET_ACCESS_KEY,
+  region: config.AWS_REGION,
+  accessKeyId: config.ACCESS_KEY_ID,
+  secretAccessKey: config.SECRET_ACCESS_KEY,
 });
 @Module({
-    imports: [
-        SqsModule.register({
-            consumers: [
-                {
-                    name: config.QUEUE, // name of the queue 
-                    queueUrl: config.QUEUE_URL, // the url of the queue
-                    region: config.AWS_REGION,
-                },
-            ],
-        }),
-        RasterModule,
-    ],
-    controllers: [],
-    providers: [MessageHandler]
+  imports: [
+    SqsModule.register({
+      consumers: [
+        {
+          name: config.QUEUE, // name of the queue
+          queueUrl: config.QUEUE_URL, // the url of the queue
+          region: config.AWS_REGION,
+        },
+      ],
+    }),
+    RasterModule,
+  ],
+  controllers: [],
+  providers: [MessageHandler],
 })
-export class MensajeModule { }
+export class MensajeModule {}
