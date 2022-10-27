9717277f3af12233809b9e026e58534f7aeb7c45
creatispoweb <creatiposweb@gmail.com>
2022-10-27 16:30:05 -0300

    comment key aws

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,8 @@ import * as AWS from 'aws-sdk';
 export class AppController {
   constructor(private readonly appService: AppService) {
     AWS.config.update({
-      accessKeyId: process.env.ACCESS_KEY_ID, //config.ACCESS_KEY_ID,
-      secretAccessKey: process.env.SECRET_ACCESS_KEY, //config.SECRET_ACCESS_KEY,
+      //accessKeyId: process.env.ACCESS_KEY_ID, //config.ACCESS_KEY_ID,
+      //secretAccessKey: process.env.SECRET_ACCESS_KEY, //config.SECRET_ACCESS_KEY,
       region: process.env.AWS_REGION, //config.AWS_REGION,
     });
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -40,10 +40,5 @@ const envFilePath: string = getEnvPath(`${__dirname}/config/envs`);
 })
 export class AppModule {
   constructor() {
-    AWS.config.update({
-      accessKeyId: process.env.ACCESS_KEY_ID, //config.ACCESS_KEY_ID,
-      secretAccessKey: process.env.SECRET_ACCESS_KEY, //config.SECRET_ACCESS_KEY,
-      region: process.env.AWS_REGION, //config.AWS_REGION,
-    });
   }
 }
