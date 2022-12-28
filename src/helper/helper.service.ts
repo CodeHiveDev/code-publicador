10aa0e2cca119a563973307f67a39c09926de888
LuisDaniel <you@example.com>
2022-12-28 11:59:59 -0300

    item for

--- src/helper/helper.service.ts
+++ src/helper/helper.service.ts
@@ -242,11 +242,13 @@ export class HelperService {
       }
       const zip = new AdmZip();
       const outputFile = './tmp/publicador/rasters/rasters.zip';
-      await items.forEach(async (item) => {
+
+      for(let item of items) {
+  
 
         zip.addLocalFile('./tmp/publicador/rasters/'+item.Key.split('/')[2])
 
-      })
+      }
 
       zip.writeZip(outputFile);
       console.log(`Created ${outputFile} successfully`);
